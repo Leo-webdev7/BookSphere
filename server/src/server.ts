6a13f1cab57789ed4926fcf47874b0235e7d4091
Leo <lshapirov2@gmail.com>
2024-11-21 21:28:28 -0600

    server.ts was refactored

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+/* import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
@@ -18,4 +18,60 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+}); */
+
+import express from 'express';
+import path from 'node:path';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import db from './config/connection.js';
+import typeDefs from './schemas/typeDefs.js';
+import resolvers from './schemas/resolvers.js';
+import { authenticateToken } from './services/auth.js';
+
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+// Middleware for parsing incoming requests 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+async function startServer() {
+  await server.start();
+
+  
+  app.use(
+    '/graphql',
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const authHeader = req.headers.authorization;
+        const user = authenticateToken(authHeader);
+        return { user }; // Add user to the GraphQL context
+      },
+    })
+  );
+
+  // Start the server once the database is connected
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`🚀 GraphQL Server ready at http://localhost:${PORT}/graphql`);
+    });
+  });
+}
+
+startServer();
