5c9b34b90cd5db84ca427bc81da0442eca241c4c
Leo <lshapirov2@gmail.com>
2024-11-23 19:21:11 -0600

    resolvers.ts & index.ts file created

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,69 @@
+import { error } from "console";
+import User from "../models/User";
+import { signToken } from "../services/auth";
+import { createUser, login } from "../controllers/user-controller";
+import exp from "constants";
+
+const resolvers {
+    Query: {
+        me: async (_,__, context) => {
+            if (!context.user) {
+                throw new Error('Not authenticated');
+            }
+            const user = await User.findById(context.user._id);
+            if (!user) {
+                throw new Error('User not found');
+            }
+            return user;
+        },
+    },
+    Mutation: {
+        login: async (_, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new Error('Can`t find this user');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new Error('Wrong password!');
+            }
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
+        },
+        addUser: async (_, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
+        },
+        saveBook: async (_, { input }, context) => {
+            if (!context.user) {
+                throw new Error('Not authenticated');
+            }
+            const updatedUser =  await User.findByIdAndUpdate(
+                context.user._id,
+                { $addToSet: { savedBooks: input } },
+                { new: true, runValidators: true }
+            );
+            if (!updatedUser) {
+                throw new Error('User not found');
+            }
+            return updatedUser;
+        },
+        removeBook: async (_, { bookId }, context) => {
+            if (!context.user) {
+                throw new Error('Not authenticated')
+            }
+            const updatedUser = await User.findByIdAndUpdate(
+                context.user._id,
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+            if (!updatedUser) {
+                throw new Error('User not found');
+            }
+            return updatedUser;
+        },
+    },
+};
+
+export default resolvers;
