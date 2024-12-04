de506e84758e8d99826c4e58f1d4247739cbaabb
Leo <lshapirov2@gmail.com>
2024-12-03 21:36:40 -0600

    path in server.ts fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -49,10 +49,10 @@ const __dirname = dirname(__filename);  // added
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
   
